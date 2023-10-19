c040785b18a7378e37273d0cc3aef1bf51cebea5
Emre <yemrecan5885@gmail.com>
2023-10-20 00:11:58 +0300

    span etiketi ile ileri ve geri tuşu oluşturuldu

--- calender.js
+++ calender.js
@@ -1,18 +1,32 @@
+const ulElement = document.querySelector('.days ul'); // Var olan ul elementini seçin
 
-    const ulElement=document.createElement('ul');
-    
-    const days = document.querySelector('.days');
+for (let j = 1; j <= 31; j++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = j.toString();
+    ulElement.appendChild(listItem);
+}
 
+const forwardButton = document.createElement('span');
+forwardButton.textContent = 'İleri';
+forwardButton.classList.add('navigation-button', 'forward-button');
 
-        for (let j = 1; j <= 31; j++) {
-            const listItem = document.createElement('li');
-            listItem.textContent = `     ${   j } `;
-            ulElement.appendChild(listItem);
-        }
+const backwardButton = document.createElement('span');
+backwardButton.textContent = 'Geri';
+backwardButton.classList.add('navigation-button', 'backward-button');
 
-        
-        days.appendChild(ulElement);
+const daysList = document.querySelector('.month ul');
 
-        if (!days.querySelector('ul')) {
-            days.appendChild(ulElement);
-        }
+forwardButton.addEventListener('click', () => {
+    // İleri tuşuna tıklandığında yapılacak işlemleri burada tanımlayabilirsiniz.
+    // Örneğin, bir sonraki ayı veya tarihi göstermek gibi.
+    console.log('İleri tuşuna tıklandı.');
+});
+
+backwardButton.addEventListener('click', () => {
+    // Geri tuşuna tıklandığında yapılacak işlemleri burada tanımlayabilirsiniz.
+    // Örneğin, bir önceki ayı veya tarihi göstermek gibi.
+    console.log('Geri tuşuna tıklandı.');
+});
+
+daysList.appendChild(backwardButton);
+daysList.appendChild(forwardButton);
