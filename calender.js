33eafd74b1d4bc8927ca0c221d00efff103c4c08
Emre <yemrecan5885@gmail.com>
2023-10-19 21:23:45 +0300

    css kodları düzenlendi ve görünüm düzenleştirildi

--- calender.js
+++ calender.js
@@ -1,20 +1,18 @@
-// Kaç tane ul eklemek istediğinizi belirleyin
-const ulCount = 1;
 
-// Belirtilen sayıda ul eklemek için bir for döngüsü kullanın
-const days = document.querySelector('.days');
+    const ulElement=document.createElement('ul');
+    
+    const days = document.querySelector('.days');
 
-for (let i = 1; i <= ulCount; i++) {
-    // Yeni bir ul (unordered list) oluşturun
-    const ulElement = document.createElement('ul');
 
-    // Her ul'a liste öğeleri (li) ekleyin
-    for (let j = 1; j <= 31; j++) {
-        const listItem = document.createElement('li');
-        listItem.textContent = ` ${j}  `;
-        ulElement.appendChild(listItem);
-    }
+        for (let j = 1; j <= 31; j++) {
+            const listItem = document.createElement('li');
+            listItem.textContent = `     ${   j } `;
+            ulElement.appendChild(listItem);
+        }
 
-    // Oluşturulan ul'ü HTML içeriğine ekleyin
-    days.appendChild(ulElement);
-}
+        
+        days.appendChild(ulElement);
+
+        if (!days.querySelector('ul')) {
+            days.appendChild(ulElement);
+        }
