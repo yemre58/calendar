87bc2d954b62d6510ade9e2eee7aa9423cb9cb69
Emre <yemrecan5885@gmail.com>
2023-10-20 20:49:39 +0300

    ay geçişleir yaoıldı ileri ve geri ile

--- calender.js
+++ calender.js
@@ -1,32 +1,67 @@
-const ulElement = document.querySelector('.days ul'); // Var olan ul elementini seçin
+const ulElement=document.createElement('ul');// const ile ulElement adında bir değişken oluşturduk ve document de de creat element il ul diy ebir etiket oluşturup içine atadık
 
-for (let j = 1; j <= 31; j++) {
-    const listItem = document.createElement('li');
-    listItem.textContent = j.toString();
-    ulElement.appendChild(listItem);
+const days= document.querySelector('.days');//dasy adında bir değişken tanımladık ve documentin  içeirndeki days a atadık
+
+for(let i=1;i<=31;i++){//i değişkeni oluştrduk ve 1 den 31 e kadar for döngüsü ile dolaştırdık
+    const listItem=document.createElement('li');//const ile listItem adında bir değişken oluşturduk ve documentte create element il ebir li etiketi oluşturup içerisie atadık 
+    listItem.textContent=`${i}`;//listItem değişkeni  yani li textConten özelliği ile içerisine for ile oluturulan  atanır 
+    ulElement.appendChild(listItem);// ul nin içeirsine li etiketi eklemeyi sağlar
+}
+days.appendChild(ulElement);//days öğesine ulElement değişkenine atanmış ul etikeni appenChil ile eklmeyi sağlar
+if(!days.querySelector('ul')){//eğer days ın içerisinde ul etiketi yokmu diye bakar yoksa aşağıdaki kodu çalıştırı
+    days.appendChild(ulElement);//dasy ın içerisne appendChil ile ul ekler
+}
+const daysList=document.querySelector('.days ul'); //const ile daysList adında bir değişken oluşturuluyor ve document tin days ul sine atanıyor
+
+const   forwarButton=document.createElement('span');//fowarButton adlı bir değişken oluşturulur ve documentte create element ile bit sspan etiketi oluşturularak içerisine atılır
+forwarButton.textContent='ileri';//fowar buttonun textine ileri yazılır
+forwarButton.classList.add('navigation-button','forwarButton');//fowar buttona calss list add(eklemek)ile iki adet sınıf eklenir 
+
+
+const backwarButton=document.createElement('span');// const  ile backwar button diye bir değişken oluşturuur ve document te de create elementile span etike oluşturulur ve içerisine atanır
+backwarButton.textContent='geri';// textine geri yazılır 
+backwarButton.classList.add('navigation-button','backwarButton');// calss list add ile iki adet sınıf eklenir 
+
+
+const months=[
+        'Ocak','Şubat','Mart','Nisan','Mayıs','HAziran','Temmuz','Ağustos','Eylül','Ekim','Kasım' ,'Aralık'    
+    ];
+
+let currentMonthsIndex=0;
+
+function updateMonth(){
+    const monthsHeader=document.querySelector('.date h1');
+    monthsHeader.textContent=months[currentMonthsIndex];
 }
 
-const forwardButton = document.createElement('span');
-forwardButton.textContent = 'İleri';
-forwardButton.classList.add('navigation-button', 'forward-button');
 
-const backwardButton = document.createElement('span');
-backwardButton.textContent = 'Geri';
-backwardButton.classList.add('navigation-button', 'backward-button');
-
-const daysList = document.querySelector('.month ul');
-
-forwardButton.addEventListener('click', () => {
-    // İleri tuşuna tıklandığında yapılacak işlemleri burada tanımlayabilirsiniz.
-    // Örneğin, bir sonraki ayı veya tarihi göstermek gibi.
-    console.log('İleri tuşuna tıklandı.');
+forwarButton.addEventListener('click',()=>{
+    if(currentMonthsIndex<months.length -1){
+        currentMonthsIndex++;
+        updateMonth();
+    }
 });
 
-backwardButton.addEventListener('click', () => {
-    // Geri tuşuna tıklandığında yapılacak işlemleri burada tanımlayabilirsiniz.
-    // Örneğin, bir önceki ayı veya tarihi göstermek gibi.
-    console.log('Geri tuşuna tıklandı.');
+backwarButton.addEventListener('click',()=>{
+      if(currentMonthsIndex>0){
+        currentMonthsIndex--;
+        updateMonth();
+      }  
+
 });
 
-daysList.appendChild(backwardButton);
-daysList.appendChild(forwardButton);
+
+
+
+
+
+
+
+daysList.appendChild(backwarButton);
+daysList.appendChild(forwarButton);
+
+
+
+
+
+
